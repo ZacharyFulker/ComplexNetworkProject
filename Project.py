95457cf7f0f804098233e16e5c58465cfe310f59
Zach Fulker <zachfulker@gmail.com>
2018-10-15 14:32:33 -0400

    update

--- Project.py
+++ Project.py
@@ -27,23 +27,83 @@ for key, value in edge_types.items():
     else:
         designs[key] = value
 
-# Bipartite pre-calculations using all edge types
-# degDesigns, degUsers = bipartite.degrees(network, users)
-# total_deg_designs = 0
-# for key, value in degDesigns.items():
-#     total_deg_designs += value
-# total_deg_users = 0
-# for key, value in degUsers.items():
-#     total_deg_users += value
+###################################################
+# Useful Bipartite statistics using all edge types#
+###################################################
 
-# Useful Bipartite statistics using all edge types
-b_density = bipartite.density(network, users)
+# Degree Measures
 numDesigns = len(designs)
 numUsers = len(users)
+b_density = len(edge_types)/(numDesigns + 2*((numUsers-1)*numDesigns))   # Each design can/has only on edge + Each user (except creator) can comment/rate every design
 avg_deg_designs = len(edge_types)/numDesigns
-avg_deg_users = len(edge_types)/numUsers
+avg_deg_users = 0
+# ADD PLOT OF DEGREE DISTRIBUTION OF DESIGNS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# ADD PLOTS OF DEGREE DISTRIBUTION CONSIDERING EACH EDGE TYPE (3 PLOTS)
 
-# Useful Bipartite statistics comments only
+# Clustering
+# No C3 clustering by definition of bipartite, elaborate and explain C4 during talk
+cluster1 = nx.square_clustering(network)  # No clustering because edges only go from users to designs
+cluster2 = bipartite.clustering(network)  # No clustering because edges only go from users to designs
+
+# Centrality Measures
+# Do these factor in directedness!!!!!!!!!!!!!!!!!!!!!!!!!???????????????????????
+closeness_centrality = bipartite.closeness_centrality(network, users)
+total_closeness_centrality = 0
+for key, value in closeness_centrality.items():
+    total_closeness_centrality += value
+avg_closeness_centrality = total_closeness_centrality/len(closeness_centrality)
+
+degree_centrality = bipartite.degree_centrality(network, users)
+total_degree_centrality = 0
+for key, value in degree_centrality.items():
+    total_degree_centrality += value
+avg_degree_centrality = total_degree_centrality/len(degree_centrality)
+
+betweenness_centrality = bipartite.betweenness_centrality(network, users)
+total_betweenness_centrality = 0
+for key, value in betweenness_centrality.items():
+    total_betweenness_centrality += value
+avg_betweenness_centrality = total_betweenness_centrality/len(betweenness_centrality)
+
+
+###################################################
+# Projection onto Users considering all edge types#
+###################################################
+
+# Degree Measures
+
+# Clustering
+
+# Centrality
+
+# Path Lengths
+
+# Communities
+
+# Nodes in Largest Component (Distribution of component size)
+
+######################################
+# Creating Null Models for comparison#
+######################################
+
+def myMultiDiGraphGenerator(numUsers, numDesigns, numRatings, numComments):
+    pass
+
+# Null Comparisons for bipartite
+
+# Null Comparisons for Users projection
+
+########################################
+# Application of a few multigraph algos#
+########################################
+
+
+
+
+
+
+
+
 
 
 
